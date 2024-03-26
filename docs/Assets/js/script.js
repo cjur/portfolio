cda603f5e9bd6c131b000092d1dcd689bfcdaf04
cjur <coreyjurasek@gmail.com>
2024-03-26 13:36:35 -0400

    Added fade in animations on scroll for home

--- docs/Assets/js/script.js
+++ docs/Assets/js/script.js
@@ -20,3 +20,25 @@ document.querySelectorAll('.grid-item').forEach(item => {
       menuIcon.textContent = "✕";
     }
   }
+
+const elements = document.querySelectorAll('.fade-in');
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1
+}
+
+const callbacks= (entries)=>{
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('active');
+    }
+  });
+}
+
+let observer = new IntersectionObserver(callbacks,options);
+
+elements.forEach(element =>{
+  observer.observe(element);
+});
